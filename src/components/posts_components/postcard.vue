4739ad29246c892aede5ccafdf4380c071673c47
emmanuel-joseph-22 <21-05559@g.batstate-u.edu.ph>
2024-07-30 21:38:44 +0800

    fetched posts

--- src/components/posts_components/postcard.vue
+++ src/components/posts_components/postcard.vue
@@ -1,17 +1,18 @@
 <template>
         <div 
-            v-for="post in fetched_posts" 
+            v-for="post in Object.keys(posts_container).reverse()"
+            :key="post" 
             class="flex flex-col border-b border-1 w-full border-gray-700 text-white hover:bg-gray-950"
             >
             <router-link :to="{ name: 'postView' }" class=" cursor-pointer">
                 <!-- header -->
                 <div class="flex flex-row w-full items-center px-4 pt-4">
-                    <h1 class="font-bold text">{{ post.userName }}</h1>
-                    <span class="ml-auto">{{ post.datePosted }}</span>
+                    <h1 class="font-bold text">{{ posts_container[post].user }}</h1>
+                    <span class="ml-auto">{{ posts_container[post].date }}</span>
                 </div>
                 <!-- post content -->
                 <div class="flex w-full items-center text-left px-4 py-1">
-                    {{ post.postContent }}
+                    {{ posts_container[post].post_content }}
                 </div>
             </router-link>
             <!-- interactions and metrics -->
@@ -19,10 +20,10 @@
                 <!-- like -->
                 <likeButton />
                 <!-- comments -->
-                <CommentButton @click="post.commentPrompt = !post.commentPrompt"/>
+                <CommentButton @click="toggle_comment(post)"/>
             </div>
             <!-- comment section -->
-            <div v-if="post.commentPrompt" class="w-11/12 mx-auto mt-1 mb-4 flex flex-row gap-2">
+            <div v-if="showComments === post" class="w-11/12 mx-auto mt-1 mb-4 flex flex-row gap-2">
                 <input type="textarea" class="w-full rounded-lg py-1 px-2 bg-darkBlue" placeholder="Enter comment...">
                 <button class="px-2 ml-1 rounded-lg bg-matcha">Send</button>
             </div>
@@ -35,52 +36,94 @@
 import likeButton from './likes.vue';
 import CommentCard from './commentCard.vue';
 import CommentButton from '../buttons/commentButton.vue';
+
+import { ref as firebase_ref, get, remove as firetrash } from 'firebase/database';
+import { HIVYE_database as db } from "@/main";
+import { useUserStore } from '@/stores/user';
 export default{
     components: {
         CommentButton,
         likeButton,
         CommentCard
     },
+    setup(){
+        const store = useUserStore();
+        const { user_id, username } = store;
+
+        return{ user_id, username, store }
+    },
+    mounted(){
+        const post_ref = firebase_ref(db, 'posts');
+        const user_liked_posts = firebase_ref(db, 'users/' + this.user_id + '/interactions/likes')
+        get(post_ref)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                this.posts_container = snapshot.val();
+                snapshot.forEach((childSnapshot) => {
+                    const post_id = childSnapshot.key
+                    const likeCount = childSnapshot.val().likes
+                    const num_likes = Object.keys(likeCount).length
+                    this.likeCount[post_id] = num_likes - 1
+                })
+            } else {
+                console.log("No data available");
+            }
+        })
+        .then(() => {
+            get(user_liked_posts)
+            .then((data) => {
+                if(data.exists()){
+                    const likes_interaction = data.val()
+                    const post_index_cont = (Object.keys(likes_interaction))
+                    // store the likes interactions into an array to intialized the like state of every liked posts
+                    post_index_cont.forEach(key => {
+                        this.user_liked[key] = true
+                    })
+                }
+            }).catch((error) => {
+                console.log("Error loading liked posts ", error )
+            })
+        })
+        .catch((error) => {
+            console.error("Error fetching posts:", error);
+        });
+        // simulate a delay to not lose fetched data when rendered
+    },
     data(){
         return {
-            // dummy data muna
-            fetched_posts: [
-                {
-                    userName: 'sushi',
-                    datePosted: '10-20-2023',
-                    postContent: 'And, too, when candles burn out And the record is faded down I know youve got people to turn to And, too, when candles burn out And the record is faded down I know youve got people to turn to',
-                    commentPrompt: false,
-                    likeCount: 0,
-                    commentCount: 0,
-                },
-                {
-                    userName: 'bussydestroyer',
-                    datePosted: '10-19-2023',
-                    postContent: 'i need coffee',
-                    commentPrompt: false,
-                    likeCount: 0,
-                    commentCount: 0,
-                },
-                {
-                    userName: 'jace',
-                    datePosted: '10-18-2023',
-                    postContent: 'cute ni vermax',
-                    commentPrompt: false,
-                    likeCount: 0,
-                    commentCount: 0,
-                },
-                {
-                    userName: 'this_bitch_crazy',
-                    datePosted: '10-17-2023',
-                    postContent: 'I havent done a cartwheel since I was nine',
-                    commentPrompt: false,
-                    likeCount: 0,
-                    commentCount: 0,
-                }
-            ],
+            username: this.username,
+            current_uid: this.user_id,
+
+            post: '',
+            imageUrl: '',
+            date: '',
+            time: '',
+            like: '',
+            comment: '',
+            comment_content: '',
+            
+            // temp storage
+            posts_container: {},
+            comments_container: {},
+            likeCount: {},
+            user_liked: {},
+            // for displaying/hiding contents on page
+            showComments: null,
+            commentLoaded: false,
+            img_post_buttons: false,
+            commentHasValue: false,
+            // initialized a key for posting new posts
+            anonymous: {
+                newpost: true
+            },
+            characterLimit: 50, //for text truncation..di nagawa
+
         }
     },
     methods: {
+        toggle_comment(index){
+            this.showComments = this.showComments === index ? null : index;
+        }
     }
 }
 </script>
